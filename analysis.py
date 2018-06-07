26e1452f4a568f049a5fdf538b23e7ddde884660
Franco Minucci <franco.minucci@esat.kuleuven.be>
2018-06-08 00:41:15 +0100

    -

--- analysis.py
+++ analysis.py
@@ -23,6 +23,33 @@ def battery(filename):
                 
     return buffer
 
+def a_nodes(filename):
+    with open(filename) as f:
+        raw = f.readlines()
+        buffer = {}
+        current = 0
+ 
+        for line in raw:
+                
+            if "n=" in line:
+                n = int(line.split('=')[1])
+                if n not in buffer:
+                    buffer[n] = []
+                current = n
+                    
+            if 'associated nodes' in line and not ("set" in line):
+                b = line.split("=")[1]
+                buffer[current].append(b)
+
+    with open('a_nodes_'+filename,'w') as ref:
+        
+        for n in buffer:
+            ref.write('n='+str(n)+'\n')
+            for b in buffer[n]:
+                ref.write(str(b)+'\n')
+                
+    return buffer
+                
 
 def isAP(filename):
     with open(filename) as f:
